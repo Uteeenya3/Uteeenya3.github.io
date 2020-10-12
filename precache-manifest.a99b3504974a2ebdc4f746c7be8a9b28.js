ef6a9a6289d8a2f7bae6f5833a06a432592a011d
Muhammad Adi <zhenkazama20@gmail.com>
2020-10-13 01:36:28 +0700

    Menyalin isi folder build

---
+++ precache-manifest.a99b3504974a2ebdc4f746c7be8a9b28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba63f0b1e28da921322c66ff726ec1b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b412d8b3c581303031b",
+    "url": "/static/css/main.898c80c5.chunk.css"
+  },
+  {
+    "revision": "6374b44df704401b3581",
+    "url": "/static/js/2.b867d86b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b867d86b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b412d8b3c581303031b",
+    "url": "/static/js/main.67eee758.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
